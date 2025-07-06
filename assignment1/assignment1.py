d271c3f453b661896f77ca8518f8f2bd74f5e21c
catbyte-io <catbyte.io@gmail.com>
2025-07-06 14:12:27 -0600

    Complete calc function and move print debugging to its own section. Use variables for errors. Catch default of all other invalid arguments.

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -3,33 +3,75 @@
 def hello():
     return "Hello!"
 
-print(hello())
 
 # Task 2: Greet with a Formatted String
 # Take one argument and return a greeting "Hello, Name!"
 def greet(name):
     return f"Hello, {name}!"
 
-print(greet("Cersei"))
 
 # Task 3: Calculator
 # Write a calc function that takes 3 arguments with default thrid argument "multiply"
 def calc(num1, num2, operator="multiply"):
+    type_error = f"You can't {operator} those values!"
+    zero_error = "You can't divide by 0!"
+
     match operator:
         case "add":
-            result = num1 + num2
+            try:
+                result = num1 + num2
+            except TypeError:
+                return type_error
         case "subtract":
             try:
                 result = num1 - num2
             except TypeError:
-                return "Cannot perform subtraction with strings"
+                return type_error
         case "multiply":
             try:
                 result = num1 * num2
             except TypeError:
-                return "Cannot multiply a string."
+                return type_error
         case "divide":
             try:
                 result = num1 / num2
+            except TypeError:
+                return type_error
             except ZeroDivisionError:
-                return "You can't divide by 0!"
+                return zero_error
+        case "modulo":
+            try:
+                result = num1 % num2
+            except TypeError:
+                return type_error
+            except ZeroDivisionError:
+                return zero_error
+        case "int_divide":
+            try:
+                result = num1 // num2
+            except TypeError:
+                return type_error
+        case "power":
+            try:
+                result = num1 ** num2
+            except TypeError:
+                return type_error
+        # Default case to catch all other arguments
+        case _:
+            return "Please choose from the following: add, subtract, multiply, divide, modulo, int_divide, or power."
+    return result
+
+
+# Task 4: 
+
+### DEBUGGING ###
+
+# Prints "Hello!"
+print(hello())
+
+# Prints "Hello, Cersei!"
+print(greet("Cersei"))
+
+# Returns "You can't subtract those values!"
+print(calc("5", 7, "subtract"))
+
