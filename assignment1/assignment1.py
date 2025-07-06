eb892802916f99521375d475a9d7d25e01cd8721
catbyte-io <catbyte.io@gmail.com>
2025-07-05 21:07:15 -0600

    Make greet function

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -2,3 +2,13 @@
 # Write a function that returns "Hello!"
 def hello():
     return "Hello!"
+
+print(hello())
+
+# Task 2: Greet with a Formatted String
+# Take one argument and return a greeting "Hello, Name!"
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Cersei"))
+
