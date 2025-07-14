53d5e2624638af350a5e6f121d7536919dd4e286
catbyte-io <catbyte.io@gmail.com>
2025-07-13 18:05:57 -0600

    Create assignment2.py and set up tasks.

---
+++ assignment2/assignment2.py
@@ -0,0 +1,14 @@
+# Task 2: Read a CSV File
+# Task 3: Find the Column Index
+# Task 4: Find the Employee First Name
+# Task 5: Find the Employee: a Function in a Function
+# Task 6: Find the Employee with a Lambda
+# Task 7: Sort the Rows by las_name Using a Lambda
+# Task 8: Create a dict for an Employee
+# Task 9: A dict of dicts, for All Employees
+# Task 10: Use the os Module
+# Task 11: Creating Your Own Module
+# Task 12: Read minutes1.csv and minutes2.csv
+# Task 13: Create minutes_set
+# Task 14: Convert to datetime
+# Task 15: Write Out Sorted List
