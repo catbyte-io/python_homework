5a5c27dafd172d0aa7e8de9299b8e3b90216b445
catbyte-io <catbyte.io@gmail.com>
2025-07-13 18:06:48 -0600

    Make diary.py and move tasks to assignment2.py.

--- assignment2/diary.py
+++ assignment2/diary.py
@@ -1,16 +1,43 @@
-# Task 1: Diary
+import traceback
 
-# Task 2: Read a CSV File
-# Task 3: Find the Column Index
-# Task 4: Find the Employee First Name
-# Task 5: Find the Employee: a Function in a Function
-# Task 6: Find the Employee with a Lambda
-# Task 7: Sort the Rows by las_name Using a Lambda
-# Task 8: Create a dict for an Employee
-# Task 9: A dict of dicts, for All Employees
-# Task 10: Use the os Module
-# Task 11: Creating Your Own Module
-# Task 12: Read minutes1.csv and minutes2.csv
-# Task 13: Create minutes_set
-# Task 14: Convert to datetime
-# Task 15: Write Out Sorted List
+
+# Task 1: Diary
+try:
+    # Track first prompt
+    first_prompt = True
+
+    # Create text string to track
+    text = ""
+
+    # Open file
+    with open("diary.txt", "w") as file:
+        # Continually prompt the user
+        while True:
+            if first_prompt:
+                text = input("What happened today? ")
+                text += "\n"
+                # Set first_prompt to false after the first prompt
+                first_prompt = False
+            else:
+                # Follow up prompt    
+                text = input("What else? ")
+                text += "\n"
+
+            # Write text to file
+            file.write(text)
+
+            # Create break condition
+            if "done for now" in text:
+                break
+
+except Exception as e:
+    trace_back = traceback.extract_tb(e.__traceback__)
+    stack_trace = list()
+    for trace in trace_back:
+        stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
+    print(f"Exception type: {type(e).__name__}")
+    message = str(e)
+    if message:
+        print(f"Exception message: {message}")
+    print(f"Stack trace: {stack_trace}")
+
