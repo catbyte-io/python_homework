4d6666571d4325d3335ee1f021e5272a056cdb22
catbyte-io <catbyte.io@gmail.com>
2025-07-28 15:32:11 -0600

    Fix syntax in print_stack_trace

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -11,7 +11,7 @@ def print_stack_trace(e):
         stack_trace = list()
         for trace in trace_back:
             stack_trace.append(f'File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}')
-            print(f"Exception type: {type(e)._name_}")
+            print(f"Exception type: {type(e).__name__}")
             message = str(e)
             if message:
                 print(f"Exception message: {message}")
