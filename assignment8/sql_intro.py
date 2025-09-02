f62c770eefba6143fc2f55c7c85c2ad6ca56149b
catbyte-io <catbyte.io@gmail.com>
2025-09-02 12:51:51 -0600

    Create sql_intro.py and do tasks 1 - 3

--- assignment8/sql_intro.py
+++ assignment8/sql_intro.py
@@ -1,7 +1,145 @@
 import sqlite3
+from datetime import datetime
+
 
 # Task 1: Create a New SQLite Database
-db_conn = sqlite3.connect('../db/magazines.db')
+with sqlite3.connect("../db/magazines.db") as conn:
 
-# Close connection
-db_conn.close()
+    conn.execute("PRAGMA foreign_keys = 1")
+
+    # Task 2: Define Database Structure
+    cursor = conn.cursor()
+
+    try:
+        # Create tables
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Publishers (
+            publisher_id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL UNIQUE
+        )
+        """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Magazines (
+            magazine_id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL UNIQUE,
+            publisher_id INTEGER,
+            FOREIGN KEY (publisher_id) REFERENCES Publishers (publisher_id)
+        )
+        """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Subscribers (
+            subscriber_id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL UNIQUE,
+            address TEXT NOT NULL
+        )
+        """)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Subscriptions (
+            subscription_id INTEGER PRIMARY KEY,
+            magazine_id INTEGER,
+            subscriber_id INTEGER,
+            expiration_date TEXT NOT NULL,
+            FOREIGN KEY (magazine_id) REFERENCES Magazines (magazine_id)
+        )
+        """)
+
+        conn.commit()
+
+    except sqlite3.IntegrityError:
+        print("An error occurred.")
+
+
+# Helper function to get an expiration date that is one year from the current day and convert to string
+def set_expiration():
+    today = datetime.now()
+    one_year = today.replace(year=today.year + 1)
+    exp_date = one_year.strftime("%Y-%m-%d")
+    return exp_date
+
+# Task 3: Populate Tables with Data
+def add_publisher(cursor, name):
+    try:
+        cursor.execute("INSERT INTO Publishers (name) VALUES (?)", (name,))
+    except sqlite3.IntegrityError:
+        print(f"{name} is already in the database.")
+
+
+def add_magazine(cursor, name, publisher):
+    try:
+        cursor.execute("SELECT * FROM Publishers WHERE name = ?", (publisher,))
+        results = cursor.fetchall()
+        if len(results) > 0:
+            publisher_id = results[0][0]
+        else:
+            print(f"Publisher {publisher} for magazine not found")
+            return
+        cursor.execute("INSERT INTO Magazines (name, publisher_id) VALUES (?,?)", (name, publisher_id))
+    except sqlite3.IntegrityError:
+        print(f"{name} is already in the database.")
+
+
+def add_subscriber(cursor, name, address):
+    try:
+        cursor.execute("INSERT INTO Subscribers (name, address) VALUES (?,?)", (name, address))
+    except sqlite3.IntegrityError:
+        print(f"{name} is already in the database.")
+
+
+def create_subscription(cursor, subscriber, address, magazine):
+    try:
+        # Try to find subscriber
+        cursor.execute("SELECT * FROM Subscribers WHERE name = ? AND address = ?", (subscriber, address))
+        results = cursor.fetchall()
+        if len(results) > 0:
+            subscriber_id = results[0][0]
+        else:
+            print(f"Subscriber {subscriber} not found.")
+            return
+        
+        # Select magazine
+        cursor.execute("SELECT * FROM Magazines WHERE name = ?", (magazine,))
+        results = cursor.fetchall()
+        if len(results) > 0:
+            magazine_id = results[0][0]
+        else:
+            print(f"Magazine titled {magazine} not found.")
+            return
+
+        # Set the expiration date for one year from now
+        expiration_date = set_expiration()
+        
+        # Add the subscription
+        cursor.execute("INSERT INTO Subscriptions (subscriber_id, magazine_id, expiration_date) VALUES (?,?,?)", (subscriber_id, magazine_id, expiration_date))
+
+    except sqlite3.IntegrityError:
+        print(f"Subscription already exists.")
+
+
+with sqlite3.connect("../db/magazines.db") as conn:
+
+    conn.execute("PRAGMA foreign_keys = 1")
+    cursor = conn.cursor()
+
+    # Add publishers
+    publishers = ["Red Line", "Yong Com", "Currents"]
+
+    for publisher in publishers:
+        add_publisher(cursor, publisher)
+
+    magazines = [("Electric Beat", "Red Line"), ("Wind Breaker", "Yong Com"), ("CyberGuardian", "Currents")]
+    
+    for magazine_name, publisher_name in magazines:
+        add_magazine(cursor, magazine_name, publisher_name)
+    
+    subscribers = [("Jody Yuhas", "1425 Sycamore, Salmon ID", "Electric Beat"), ("Klint Hondel", "829 Five Mile Creek, Taos NM", "CyberGuardian"), ("Georgia Harris", "1355 Tamarack Drive, Tuscon AZ", "Wind Breaker")]
+
+    for sub_name, sub_address, magazine_name in subscribers:
+        add_subscriber(cursor, sub_name, sub_address)
+        create_subscription(cursor, sub_name, sub_address, magazine_name)
+
+
+# Task 4: Write SQL Queries
+
