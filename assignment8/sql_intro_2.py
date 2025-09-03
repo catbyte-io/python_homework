070cd679de94ae31f0c7d0b513dae067983d1227
catbyte-io <catbyte.io@gmail.com>
2025-09-02 21:19:44 -0600

    fix agg and the df sorted.

--- assignment8/sql_intro_2.py
+++ assignment8/sql_intro_2.py
@@ -11,14 +11,14 @@ with sqlite3.connect("../db/lesson.db") as conn:
 
 # Add a column to the DataFrame called "total". This is the quantity times the price. (This is easy: df['total'] = df['quantity'] * df['price'].) Print out the first 5 lines of the DataFrame to make sure this works.
 df['total'] = df['quantity'] * df['price']
-print(f"Added total column:\n{df.head(5)}")
+print(f"\nAdded total column:\n{df.head(5)}\n")
 
 # Add groupby() code to group by the product_id. Use an agg() method that specifies 'count' for the line_item_id column, 'sum' for the total column, and 'first' for the 'product_name'. Print out the first 5 lines of the resulting DataFrame. Run the program to see if it is correct so far.
-df_groupby = df.groupby('product_id').agg({'line_item_id':['count'], 'total': ['sum'], 'product_name': ['first']})
-print(f"groupby and agg:\n{df_groupby.head(5)}")
+df_groupby = df.groupby('product_id').agg({'line_item_id':'count', 'total': 'sum', 'product_name': 'first'})
+print(f"groupby and agg:\n{df_groupby.head(5)}\n")
 
 # Sort the DataFrame by the product_name column.
-df_sorted = df_groupby.sort_values(by=[('product_name', 'first')])
+df_sorted = df.sort_values(by='product_name')
 print(f"Sorted by product_name:\n{df_sorted.head(5)}")
 
 # Add code to write this DataFrame to a file order_summary.csv, which should be written in the assignment8 directory. Verify that this file is correct.
