41c6c775ba2c206b3f3cef7eb2be316b021809e8
catbyte-io <catbyte.io@gmail.com>
2025-09-12 12:19:16 -0600

    Create owasp_top_ten.py

---
+++ assignment10/owasp_top_ten.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+
+# Task 6: Scraping Structured Data
