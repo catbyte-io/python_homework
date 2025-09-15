27c0d464746a96bb0f4a69561fff3e89eaab1e95
catbyte-io <catbyte.io@gmail.com>
2025-09-15 08:33:17 -0600

    Fix how order_id is returned

--- assignment9/advanced_sql.py
+++ assignment9/advanced_sql.py
@@ -79,7 +79,7 @@ with sqlite3.connect("../db/lesson.db") as conn:
         date = datetime.now().strftime("%Y-%m-%d")
 
         # Create order
-        cursor.execute("INSERT INTO orders (customer_id, employee_id, date) VALUES (?,?,?) RETURNING (order_id)", (customer_id, employee_id, date))
+        cursor.execute("INSERT INTO orders (customer_id, employee_id, date) VALUES (?,?,?) RETURNING order_id", (customer_id, employee_id, date))
         order_id = cursor.fetchone()[0]
 
         # Prepare data by creating tuples
@@ -112,7 +112,7 @@ with sqlite3.connect("../db/lesson.db") as conn:
     # Task 4: Aggregation with HAVING
     # Find all employees associated with more than 5 orders.  You want the first_name, the last_name, and the count of orders. 
     sql_statement_3 = """
-        SELECT e.first_name, e.last_name, COUNT(o.order_id) AS num_orders
+        SELECT e.first_name, e.last_name, COUNT (o.order_id) AS num_orders
         FROM employees e JOIN orders o ON e.employee_id = o.employee_id
         GROUP BY e.employee_id
         HAVING num_orders > 5;
